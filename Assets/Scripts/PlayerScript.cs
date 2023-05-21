5222041d00a617e5f5844e7172f37667d5a93c7d
thaDameer <38245328+thaDameer@users.noreply.github.com>
2023-05-21 20:10:20 +0200

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    
-    
-    private void Start()
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float moveSpeed = 5;
+    Vector3 inputDir
     {
-        throw new NotImplementedException();
+        get
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+            return input;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputDir != Vector3.zero)
+        {
+            _rigidbody.velocity = inputDir * moveSpeed;
+            var rot = Quaternion.LookRotation(inputDir,Vector3.up);
+            rot.y = 0;
+            transform.rotation = rot;
+        }
     }
 }
